596d4e1ead0b7fa260f6eb1dc5f8e8dba71d65b0
marshallb93 <marshallbradley93@gmail.com>
2015-11-02 19:55:47 +0000

    Initial commit

---
+++ src/raspberry_python.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Marshall"
+__date__ ="$02-Nov-2015 19:43:50$"
+
+if __name__ == "__main__":
+    print "Hello World"
