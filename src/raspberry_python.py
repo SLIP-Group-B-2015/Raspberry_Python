dda453de1c3f6ef60aadb277c1f44066a20aa6d4
marshallb93 <marshallbradley93@gmail.com>
2015-11-03 15:58:28 +0000

    Testing Python -> Java sockets

--- src/raspberry_python.py
+++ src/raspberry_python.py
@@ -1,9 +1,44 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 __author__="Marshall"
-__date__ ="$02-Nov-2015 19:43:50$"
 
-if __name__ == "__main__":
-    print "Hello World"
+import nxppy
+#import pygatt
+import time
+import socket
+import sys
+
+
+host = "localhost"
+port = 12000
+
+soc = socket.socket()
+soc.connect((host, port))
+
+i = 0
+while True:
+    soc.sendall("hello " + str(i) + " \n")
+    time.sleep(1)
+    i += 1
+    
+
+"""
+mifare = nxppy.Mifare()
+
+# Print card UIDs as they are detected
+while True:
+    try:
+        uid = mifare.select()
+        print(uid)
+    except nxppy.SelectError:
+        # SelectError is raised if no card is in the field.
+        pass
+
+    time.sleep(1)
+
+MACADD = 'D2:70:C8:15:2B:97'
+
+# pygatt.util.reset_bluetooth_controller()
+dev = pygatt.pygatt.BluetoothLEDevice(MACADD, app_options='-t random')
+dev.connect()
+do = dev.char_read_uuid('0xA001')
+dev.disconnect()"""
+
